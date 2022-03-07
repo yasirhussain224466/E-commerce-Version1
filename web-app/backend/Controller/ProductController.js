55fa763e2b1a4478e33df279d0667d66a0818998
Muhammad Yasir Hussain <76113598+yasirhussain224466@users.noreply.github.com>
2022-03-07 14:07:33 +0500

    2:07 Monday

--- web-app/backend/Controller/ProductController.js
+++ web-app/backend/Controller/ProductController.js
@@ -1,47 +1,24 @@
 const Product = require("../Model/ProductModel");
+const CatchAsync = require("../Utils/CatchAsync");
+const AppError = require("../Utils/ErrorHandler");
+const APIFeatures = require("../Utils/ProductFeatures");
 
-exports.createProduct = async (req, res, next) => {
-  try {
-    const newProduct = await Product.create(req.body);
-    res.status(201).json({
-      status: "success",
-      data: {
-        product: newProduct,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+// only Admin can access the route of Create, Update and Delete the Product
 
-exports.getAllProducts = async (req, res, next) => {
-  try {
-    const products = await Product.find();
-    res.status(200).json({
-      status: "success",
-      items: `${products.length}`,
-      Data: products,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+exports.createProduct = CatchAsync(async (req, res, next) => {
+  const newProduct = await Product.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      product: newProduct,
+    },
+  });
+});
 
-exports.updateProduct = async (req, res, next) => {
-  const ConfirmProduct = await Product.findById(req.params.id);
-  if (!ConfirmProduct) {
-    res.status(500).json({
-      status: "fail",
-      message: "No Product found with that ID",
-    });
-  }
-  const updateProduct = await Product.findByIdAndUpdate(
+exports.updateProduct = CatchAsync(async (req, res, next) => {
+  // const confirmProduct = await Product.findById(req.params.id);
+  // if (!confirmProduct) throw new Error("Invalid ID, Product Not found");
+  const updatedProduct = await Product.findByIdAndUpdate(
     req.params.id,
     req.body,
     {
@@ -49,43 +26,50 @@ exports.updateProduct = async (req, res, next) => {
       runValidators: true,
     }
   );
-  if (!updateProduct) {
-    res.status(500).json({
-      status: "fail",
-      message: "No Product found with that ID",
-    });
+  if (!updatedProduct) {
+    return next(new AppError("No Product found with that ID", 404));
   } else {
     res.status(200).json({
       status: "success",
-      data: updateProduct,
+      data: updatedProduct,
     });
   }
-};
+});
 
-exports.DeleteProduct = async (req, res, next) => {
+exports.DeleteProduct = CatchAsync(async (req, res, next) => {
   const deleteProduct = await Product.findByIdAndRemove(req.params.id);
   if (!deleteProduct) {
-    res.status(500).json({
-      status: "fail",
-      message: "No Product found with that ID",
-    });
+    return next(new AppError("No Product found with that ID", 404));
   }
   res.status(204).json({
     status: "success",
     data: deleteProduct,
   });
-};
+});
 
-exports.getProductDetail = async (req, res, next) => {
-  const productDetail = await Product.findById(req.params.id);
-  if (!productDetail) {
-    res.status(500).json({
-      status: "fail",
-      message: "No Product found with that ID",
-    });
+exports.getAllProducts = CatchAsync(async (req, res, next) => {
+  const productQuery = new APIFeatures(Product.find(), req.query)
+    .filter()
+    .limitFields()
+    .paginate()
+    .search()
+    .sort();
+  const products = await productQuery.query;
+  console.log(req.query);
+  res.status(200).json({
+    status: "success",
+    items: `${products.length}`,
+    Data: products,
+  });
+});
+
+exports.getProductDetail = CatchAsync(async (req, res, next) => {
+  const productDetails = await Product.findById(req.params.id);
+  if (!productDetails) {
+    return next(new AppError("No Product found with that ID", 404));
   }
   res.status(200).json({
     status: "success",
-    data: productDetail,
+    data: productDetails,
   });
-};
+});
--- web-app/backend/index.js
+++ web-app/backend/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const morgan = require("morgan");
-// Routes import
 const product = require('./Routes/ProductRoute')
+const GlobalErrorHandler = require('./Controller/ErrorController')
 
 
 app.use(express.json());
@@ -13,5 +13,22 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/v1', product)
+
+app.all("*", (req, res, next) => {
+  // console.log("Wrong Url")
+  // res.status(500).json({
+  //   status: 'fail',
+  //   message: `Invalid URl ${req.originalUrl} `
+  // })
+  // next()
+
+  // const err = new Error(`Invalid URl ${req.originalUrl}`)
+  // err.status = 'fail'
+  // err.statusCode = 400
+  // next(err)
+
+  next(new AppError(`Invalid URl ${req.originalUrl}`), 500);
+});
+app.use(GlobalErrorHandler);
 
 module.exports = app
